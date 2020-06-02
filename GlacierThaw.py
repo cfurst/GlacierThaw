5b9ca24ba296ab47ccffb41456de29c97d013330
Carl Yamamoto-Furst <Carl Yamamoto-Furst@DESKTOP-VR3QPPI.fios-router.home>
2020-06-02 18:39:44 -0400

    basically working..first draft

--- GlacierThaw.py
+++ GlacierThaw.py
@@ -1,8 +1,12 @@
-import boto3
 import sys
 
+import boto3
 
 s3 = boto3.client('s3')
+DEFAULT_NUMBER_OF_DAYS_AVAILABLE = 14
+DEFAULT_TIER = 'Bulk'
+
+
 def getMetadata(bucket, key):
     """
     returns true when there is metadata available for the object
@@ -14,28 +18,42 @@ def getMetadata(bucket, key):
         return None
     return metadata
 
-def getObjectList(bucket, key, continuationToken = None):
+
+def getObjectList(bucket, key, continuationToken=None):
     """
-    will return the first level of stuff in the key
+    will return the list of stuff given the prefix of key
     """
-    if not bucket and key:
-        return {}
-    else:
-        if not continuationToken:
+    if not continuationToken:
             return s3.list_objects_v2(Bucket=bucket, Prefix=key)
-        return s3.list_objects_v2(Bucket=bucket, Prefix=key, ContinuationToken=continuationToken)
+    return s3.list_objects_v2(Bucket=bucket, Prefix=key, ContinuationToken=continuationToken)
+
+    
+def makeGlacierRequest(awsObject, sourceBucket, tier=DEFAULT_TIER, numberOfDaysAvailable=DEFAULT_NUMBER_OF_DAYS_AVAILABLE, destinationBucket='', destinationPrefix=''):
+    try: 
+        restore_request = {
+            'GlacierJobParameters': {
+                "Tier": tier
+            },
+            'Days': numberOfDaysAvailable,
+        }
+       
+        s3.restore_object(Bucket=bucket, Key=awsObject['Key'], RestoreRequest=restore_request)
+    except s3.exceptions.ObjectAlreadyInActiveTierError as e:
+        print("Couldn't restore:", awsObject['Key'], e)
+    
+
 try:
     bucket = sys.argv[1]
     key = None
     if len(sys.argv) >= 3:
         key = sys.argv[2]
-    print("bucket: {bucket}, key: {key}".format(bucket=bucket, key=key))
-    print(getObjectList(bucket, key));
-       
-    
-
+    print("bucket: ", bucket, "key: ", key)
+    if not key:
+        key = ''
+    objectList = getObjectList(bucket, key)
+    for awsObject in objectList['Contents']:
+        if awsObject['StorageClass'] == 'GLACIER':
+            makeGlacierRequest(awsObject, bucket)
 except IndexError:
     print('Invalid Arguments! I need a bucket at least')
-
-
 
