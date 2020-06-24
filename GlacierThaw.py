c2646925caf4a68b316d78e8470d102e2d98e8fe
Skr00b@!! <carl.furst@seefurst.com>
2020-06-24 00:49:46 -0400

    Destination didn't work.. I think its for select operations only so I implemented restore tier and duration instead. (#1)
    * first attempt as destination location
    
    * Got destination bucket/prefix working. Not verified.
    
    * Ok it really wasn't working.
    
    * still not working
    
    * fleshed out help and destination request.
    
    * took out destination bucket and prefix. Only used for select requests
    
    * fixed help
    
    * back to working without destination
    
    * comments
    
    * full bucket retreival working
    
    * doc corrections

--- GlacierThaw.py
+++ GlacierThaw.py
@@ -1,6 +1,16 @@
-import sys
-
+#!
 import boto3
+import argparse
+from symbol import argument
+import botocore
+
+
+
+argument_parser = argparse.ArgumentParser()
+argument_parser.add_argument("bucket", help="The name of the s3 bucket that is to be the root of your request. Ex. if your files live at s3://my-awesome-bucket/foo/bar the bucket name would be 'my-awsome-bucket'")
+argument_parser.add_argument("--prefix", help="A prefix or key to use as the base URL for your request. Ex. if your files live in the 'foo/bar' directory, then 'foo/bar' is the prefix. If you want to just request one file to be moved, use the path of the file as the prefix Ex. foo/bar/myFile.txt. If you leave out a prefix, you will make a request for the entire bucket. Please be careful. Ctrl-C is your friend.", default='')
+argument_parser.add_argument("--request-tier", help="The type of glacier request you would like to make, Choices are 'Standard', 'Expedited' and 'Bulk' (default). Each has purposes and limitations, please consult the AWS documentation for more information", dest="tier", choices=['Bulk','Standard','Expedited'], default='Bulk')
+argument_parser.add_argument("--duration", help="The number of days you wish to have the object available before being returned to Glacier storage; defaults to two weeks.", type=int, default=14)
 
 s3 = boto3.client('s3')
 DEFAULT_NUMBER_OF_DAYS_AVAILABLE = 14
@@ -20,9 +30,9 @@ def getMetadata(bucket, key):
     
 
     
-def makeGlacierRequest(awsObject, source_bucket, tier=DEFAULT_TIER, numberOfDaysAvailable=DEFAULT_NUMBER_OF_DAYS_AVAILABLE, destinationBucket='', destinationPrefix=''):
+def makeGlacierRequest(aws_object, source_bucket, tier=DEFAULT_TIER, number_of_days_available=DEFAULT_NUMBER_OF_DAYS_AVAILABLE):
     """
-    Creates a glacier request for a deafult period of 14 days
+    Creates a glacier request for a default period of 14 days
     """
     # TODO: implement destination bucket and prefix.
     try: 
@@ -30,30 +40,34 @@ def makeGlacierRequest(awsObject, source_bucket, tier=DEFAULT_TIER, numberOfDays
             'GlacierJobParameters': {
                 "Tier": tier
             },
-            'Days': numberOfDaysAvailable,
+            'Days': number_of_days_available,
         }
-        s3.restore_object(Bucket=source_bucket, Key=awsObject['Key'], RestoreRequest=restore_request)
-    except s3.exceptions.ObjectAlreadyInActiveTierError as e:
-        print("Couldn't restore:", awsObject['Key'], " possibly already restored...")
+        # TODO: Specify a queue to read restore notifications from. 
+        s3.restore_object(Bucket=source_bucket, Key=aws_object['Key'], RestoreRequest=restore_request)
+    except (s3.exceptions.ObjectAlreadyInActiveTierError, botocore.exceptions.ClientError) as e:
+        print("Couldn't restore:", aws_object['Key'], " possibly already restored...", e)
+    else:
+        print(aws_object['Key'], " will be restored for {} days.".format(number_of_days_available))
     
 def main():
     try:
-        bucket = sys.argv[1].strip()
-        key = None
-        if len(sys.argv) >= 3:
-            key = sys.argv[2].strip()
+        input_args = argument_parser.parse_args()
+        bucket = input_args.bucket
+        key = input_args.prefix
+        kwargs = {
+            "tier": input_args.tier,
+            "number_of_days_available": input_args.duration
+        }
+        
         print("bucket: ", bucket, "key: ", key)
-        if not key:
-            key = ''
+        
         object_iter = paginator.paginate(Bucket=bucket, Prefix=key)
     
         for page in object_iter:
             for awsObject in page['Contents']:
                 if awsObject['StorageClass'] == 'GLACIER':
-                  makeGlacierRequest(awsObject, bucket)
-                  print(awsObject['Key'], " will be restored for 14 days.")
-    except IndexError:
-        print('Invalid Arguments! I need a bucket at least')
+                  makeGlacierRequest(awsObject, bucket, **kwargs)
+                  
     except KeyError:
         print('prefix: {} did not return any objects.'.format(key))
     
--- README.md
+++ README.md
@@ -10,6 +10,28 @@ Any changes to this software under the MIT License terms does not terminate the 
 ```python
 python GlacierThaw <Bucket> [Prefix or key]
 ```
-will create a bulk request to move all objects in `Bucket` with `Prefix` or file with name `key` from the glacier storage tier temporarily for 14 days.
+will create a bulk request to move all objects in `Bucket` with `Prefix` or file with name `key` from the glacier storage tier to standard tier temporarily for 14 days.
 
-If you leave out a prefix or key, it will make a request for all objects in the bucket. Please use carefully.
+If you leave out the `prefix or key` option, it will make a request for all objects in the bucket. Please use carefully.
+
+## Retreiving files from s3
+
+**From the console:**
+
+Once the transfer request has been expedited, your files will be ready to view. The timing as to when the request completes depends on what type of request you make (deafult is bulk). You can find out more about the type of Glacier restore requests and their timing [here](https://docs.aws.amazon.com/AmazonS3/latest/dev/restoring-objects.html#restoring-objects-retrieval-options).
+
+If you go to the console and select a file that was transfered from the Glacier teir, you will see that you can now open the file and download it. You should see a `Restore Expiration` date and time when viewing the file's details. You will be able to download the file until that date, after which the file will be returned to the Glacier storage tier.
+
+**AWS CLI**
+
+If you would like to recursively download your files that you've just moved out of Glacier you can try the following command:
+
+```bash
+$ aws cli s3 cp --recursive --force-glacier-transfer s3://bucket-name/prefix/name .
+```
+
+You will need the `--force-glacier-transfer` option because of an issue with the s3 api as documented [here](https://github.com/aws/aws-cli/issues/5268)
+
+**Restoring an object to the standard storage teir**
+
+If you want to move an object out of Glacier and back into the standard tier (or any other storage tier, really) you can review [this blog post](https://aws.amazon.com/premiumsupport/knowledge-center/restore-s3-object-glacier-storage-class/) for more information. You basically have to use a method of s3 copying to basically copy the file in place.
